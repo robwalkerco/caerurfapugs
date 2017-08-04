8ce193fe954a019ba9d8d5ba229f4739edfb3d86
Rob Walker <rob@papertrail.io>
2017-08-04 22:59:10 +0100

    disable gatsby-plugin-catch-links

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-catch-links`,
+    // `gatsby-plugin-catch-links`,
   ],
 }
