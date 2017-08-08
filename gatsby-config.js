58cf6c2107ac23c60e068791c2db823f475dd40c
Rob Walker <rob@papertrail.io>
2017-08-08 22:10:08 +0100

    Add offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,29 @@
 module.exports = {
   siteMetadata: {
-    title: `Caerurfa Pugs: North Yorkshire Pug Breeders`,
+    title: 'Caerurfa Pugs: North Yorkshire Pug Breeders',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-2075901-4',
       },
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Caerurfa Pugs',
+        short_name: 'Caerurfa Pugs',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'minimal-ui',
+        icons: [
+        ],
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 }
